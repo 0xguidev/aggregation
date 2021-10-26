94dc5b0214a15c91ff38dd98575ac84ab3ebda94
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-10-26 08:59:38 -0300

    desafio5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -9,7 +9,7 @@ db.movies.aggregate([
   {
     $match: {
       countries: "USA",
-      "tomatoes.viewer.rating": { $gte: 3},
+      "tomatoes.viewer.rating": { $gte: 3 },
       cast: { $exists: true },
     },
   },
