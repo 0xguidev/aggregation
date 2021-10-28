8b2bc67226e1eb64f9fe1194e72bf1f248ef82c4
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-10-28 20:55:52 -0300

    desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,16 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $ne: "" } } },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+    },
+  },
+
+]);
