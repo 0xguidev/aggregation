cb4efe7be62d3894e244eaae40332f5e273a58b6
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-11-04 14:15:09 -0300

    desafio12

---
+++ challenges/desafio12.js
@@ -0,0 +1,24 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        diaDaSemana: { $dayOfWeek: "$startTime" },
+        nomeEstacao: "$startStationName",
+      },
+      total: { $sum: 1.0 },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      nomeEstacao: "$_id.nomeEstacao",
+      total: "$total",
+    },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
